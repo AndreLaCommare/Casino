33a5519bf2ccaa7956d4c8fc0ee9b7464dfdf748
andre_lacommare <alacommaresb@gmail.com>
2023-05-22 14:09:36 -0700

    UserTest Green Postman test successful

--- CasinoREST/src/main/java/casino/services/AuthServiceImpl.java
+++ CasinoREST/src/main/java/casino/services/AuthServiceImpl.java
@@ -2,10 +2,11 @@ package casino.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import casino.entities.User;
 import casino.repositories.UserRepository;
-
+@Service
 public class AuthServiceImpl implements AuthService {
 
 	
@@ -17,10 +18,10 @@ public class AuthServiceImpl implements AuthService {
 	@Override
 	public User register(User user) {
 		// TODO Auto-generated method stub
-//		String encrypted = encoder.encode(user.getPassword());
-//		user.setPassword(encrypted);
-//		user.setEnabled(true);
-//		user.setRole("standard");
+		String encrypted = encoder.encode(user.getPassword());
+		user.setPassword(encrypted);
+		user.setEnabled(true);
+		user.setRole("standard");
 		return userRepo.saveAndFlush(user);
 	}
 
